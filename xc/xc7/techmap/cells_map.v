2b3762c85054cfa669558a01a64f43802707f919
Xiretza <xiretza@xiretza.xyz>
2021-03-05 11:13:46 +0100

    Fix PLLE2_BASE techmap

--- xc/xc7/techmap/cells_map.v
+++ xc/xc7/techmap/cells_map.v
@@ -4142,7 +4142,7 @@ endmodule
 module PLLE2_BASE
 (
 input         CLKFBIN,
-input         CLKIN,
+input         CLKIN1,
 
 output        CLKFBOUT,
 output        CLKOUT0,
@@ -4153,6 +4153,7 @@ output        CLKOUT4,
 output        CLKOUT5,
 
 input         RST,
+input         PWRDWN,
 output        LOCKED
 );
 
@@ -4239,7 +4240,7 @@ output        LOCKED
   _TECHMAP_REPLACE_
   (
   .CLKFBIN(CLKFBIN),
-  .CLKIN1(CLKIN),
+  .CLKIN1(CLKIN1),
   .CLKINSEL(1'b1),
 
   .CLKFBOUT(CLKFBOUT),
@@ -4250,7 +4251,7 @@ output        LOCKED
   .CLKOUT4(CLKOUT4),
   .CLKOUT5(CLKOUT5),
 
-  .PWRDWN(1'b0),
+  .PWRDWN(PWRDWN),
   .RST(RST),
   .LOCKED(LOCKED),
 
